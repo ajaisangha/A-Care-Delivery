be4b23d96facafa20d183959d500c1943913242f
ajaisangha <ajaisangha@gmail.com>
2025-10-02 03:33:52 -0400

    home page update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // script.js
 
-// Example: Alert on clicking "Book a Delivery"
 document.addEventListener("DOMContentLoaded", function () {
   const bookBtn = document.querySelector(".btn-lg");
   if (bookBtn) {
